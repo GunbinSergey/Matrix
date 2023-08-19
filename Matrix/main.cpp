5297126a1e85ee78aa4245d0bc39927b54ff59c8
Сергей Гунбин <Gunbin2308@gmail.com>
2023-08-19 13:56:47 +0500

    Написана функция определителя на динамических массиввах

---
+++ Matrix/main.cpp
@@ -0,0 +1,99 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QDebug>
+
+void write_arr(int** arr, int r);
+int** mini_matrix(int** mtrx, int rang, int ban_col);
+
+
+int Opred(int** mtr, int n)
+{
+
+    if (n == 2)
+    {
+        return mtr[0][0] * mtr[1][1] - mtr[1][0] * mtr[0][1];
+    }
+
+    int n2 = n -1;
+    int** frs = mini_matrix(mtr, n, 0);
+    int** sec = mini_matrix(mtr, n, 1);
+    int** thr = mini_matrix(mtr, n, 2);
+    if (n == 3)
+    {
+
+        return mtr[0][0] * Opred(frs, n2) - mtr[0][1] * Opred(sec, n2) + mtr[0][2] * Opred(thr,n2);
+    }
+    else if (n == 4)
+    {
+         int** frt = mini_matrix(mtr, n, 2);
+         return mtr[0][0] * Opred(frs, n2) - mtr[0][1] * Opred(sec, n2) + mtr[0][2] * Opred(thr,n2) * mtr[0][3] * Opred(frt, n2);
+    }
+    return 0;
+}
+
+
+int** mini_matrix(int** mtrx, int rang, int ban_col)
+{
+    //Матрица с пониженным рангом для расчёта опредилителя старшей
+    int new_r = rang - 1;
+    int** new_m = new int* [new_r];
+
+    for(int i =0; i < new_r; ++i)
+    {
+        *new_m = new int[new_r];
+        new_m++;
+    }
+    new_m -= (new_r);
+
+
+
+    //Проход по строкам
+    for (int i = 1; i < rang; ++i)
+    {
+        int row = 0;
+        for (int j = 0; j < rang; ++j)
+        {
+            if (j == ban_col)
+                continue;
+            new_m[i-1][row] = mtrx[i][j];
+            //qDebug() << new_m[i-1][row] << " " << mtrx[i][j];
+            row++;
+        }
+    }
+
+    return new_m;
+}
+
+void write_arr(int** arr, int r)
+{
+    for (int i = 0; i < r; ++i)
+    {
+        for (int j = 0; j < r; ++j)
+        {
+            qDebug() << arr[i][j];
+        }
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    int n = 3;
+    int **pmas = new int*[n];
+    *pmas = new int[n] {1,4,8};
+    pmas++;
+    *pmas = new int[n] {2,5,2};
+    pmas++;
+    *pmas = new int[n] {7,8,13};
+    pmas -= (n - 1);
+
+    qDebug() << Opred(pmas, n);
+    return a.exec();
+}
+
+
